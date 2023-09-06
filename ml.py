6ca58f492b7164b3d680d424c9c264cb1e894d22
gomezgaston <gaston.gomez.15@hotmail.com>
2023-09-05 22:49:20 -0300

    fixing var names

--- ml.py
+++ ml.py
@@ -4,9 +4,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 cv = CountVectorizer(max_features=45, stop_words="english")
 
-df_ML_reduced = pd.read_csv(r"data_api\datos_ML_reduced.csv")
-df_ML = pd.read_csv(r"data_api\datos_ML.csv")
-
 
 #esta es la funcion con la que implementariamos el modelo, como vemo hacer unas recomendaciones bastantes acertadas sin caer en la reduncancia.
 #dentro de los try y except no se puede llamar a variables externas a la funcion por o que me veo forzado a correr siempre la carga del modelo.
@@ -32,22 +29,27 @@ def recommend(id_item):
     
     
     except IndexError:    
-
-        df_ML_reduced = pd.read_csv(r"data_api\datos_ML_reduced.csv")
+        
         df_ML = pd.read_csv(r"data_api\datos_ML.csv")
+        
         new_game = df_ML[df_ML["item_id"] == id_item]
+        
         df_combined = pd.concat([df_ML,new_game])
+        
         vectors = cv.fit_transform(df_combined["tags"]).toarray()
+        
         similarity=cosine_similarity(vectors)
+        
         idx = df_combined[df_combined["item_id"] == id_item].index[0]
+        
         distances = similarity[idx]   
+        
         jueguito = sorted(list(enumerate(distances)), reverse=True, key= lambda x:x[1])[1:6]
+        
         respon = []
         for i in jueguito:
             respon.append(df_combined.iloc[i[0]].title)
         return {"recomendaciones": respon}
     
     
-#print(type(recommend(10)))
-
             
